711913f458e4a126659f65a9e5fc778092b1e641
Shamim <sehsan@carreirdirect.com>
2021-02-09 14:59:27 +0600

    Initial structure with create-react-app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase/app";
+firebase.initializeApp({
+  apiKey: "AIzaSyAIx5BjBIXvQwb_m1JsTYFs6ZxzXfSKP-Q",
+  authDomain: "reversi-aa2e8.firebaseapp.com",
+  projectId: "reversi-aa2e8",
+  storageBucket: "reversi-aa2e8.appspot.com",
+  messagingSenderId: "397175895344",
+  appId: "1:397175895344:web:e0680cbd6dd6e3955844f5",
+  measurementId: "G-LQ3Q7MHFTM",
+});
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
